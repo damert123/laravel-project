e1bc87047557b8361eceea1a186482b0bdd8d01f
BlessedCare <damert337@gmail.com>
2024-05-18 16:06:22 +0400

    Добавили composer

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+
+git pull origin master
+
+php8.1 composer.phar install --no-dev --optimize-autoloader
+
+php8.1 artisan down
+
+
+php8.1 artisan migrate --force
+
+php8.1 artisan config:cache
+
+php8.1 artisan view:cache
+
+php8.1 artisan route:cache
+
+php8.1 artisan up
+
+
+
